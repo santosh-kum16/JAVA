b3ff3c4edd934ab4868fbd6bb9a2e26007f8863e
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2024-01-04 00:20:33 +0530

    Create LaunchDemo.java

---
+++ Basics of Java/LaunchDemo.java
@@ -0,0 +1,18 @@
+class Dog
+{
+	String name;
+	String color;
+	int cost;
+}
+
+
+class LaunchDemo 
+{
+	public static void main(String[] args) 
+	{
+		Dog d= new Dog();
+		System.out.println(d.name);
+		System.out.println(d.color);
+		System.out.println(d.cost);
+	}
+}
