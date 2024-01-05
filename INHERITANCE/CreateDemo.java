aee8080e179dc00e17f5cc877cad90f25c8ed0bc
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2024-01-05 23:59:01 +0530

    Create CreateDemo.java

---
+++ INHERITANCE/CreateDemo.java
@@ -0,0 +1,20 @@
+class Demo1 
+{
+	
+}
+class Demo2
+{
+
+}
+class Demo3 extends Demo1,Demo2
+{
+
+}
+
+class LaunchDemo
+{
+	public static void main(String args[])
+	{
+		Demo3 d=new Demo3();
+	}
+}
