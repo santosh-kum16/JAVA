0da2288f35eab46166b16985aec17403d40dbb1a
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2024-01-06 00:05:18 +0530

    Create MultiLevelDemo.java

---
+++ INHERITANCE/MultiLevelDemo.java
@@ -0,0 +1,19 @@
+class Demo4
+{
+
+}
+class Demo5 extends Demo4
+{
+
+}
+
+class Demo6 extends Demo5
+{
+
+}
+
+class MultiLevelDemo
+{
+	public static void main(String args[])
+		Demo4 d=new Demo4();
+}
