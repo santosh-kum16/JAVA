d927ce1c8ecc10baf665309250c7bff2fd03964c
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2024-03-09 23:34:30 +0530

    Create ClientFiveTimesCommunication.java

---
+++ ADVANCE JAVA/NETWORKING/ClientFiveTimesCommunication.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+class Client5timesComm 
+{
+	public static void main(String[] args)throws Exception 
+	{
+		Socket csoc = new Socket("localhost",4000);
+		OutputStream os = csoc.getOutputStream();
+		DataOutputStream dos = new DataOutputStream(os);
+		InputStream is = csoc.getInputStream();
+		DataInputStream dis = new DataInputStream(is);
+		Scanner scan = new Scanner(System.in);
+		for (int i=0;i<5 ;i++ )
+		{
+			String msg = scan.nextLine();
+			dos.writeUTF(msg);
+			String reply = dis.readUTF();
+			System.out.println(reply);
+		}
+		
+	}
+}
