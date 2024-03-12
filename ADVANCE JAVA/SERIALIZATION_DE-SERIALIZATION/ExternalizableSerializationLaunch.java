d6f24b09666419a4ca4c46a2e2a306f6cde428d5
Santosh Kumar <62263024+santosh-kum16@users.noreply.github.com>
2024-03-12 23:58:07 +0530

    Create ExternalizableSerializationLaunch.java

---
+++ ADVANCE JAVA/SERIALIZATION_DE-SERIALIZATION/ExternalizableSerializationLaunch.java
@@ -0,0 +1,46 @@
+import java.io.*;
+class Cricketer implements Serializable
+{
+	String name;
+	int runs;
+	float avg;
+	Cricketer(String name,int runs,float avg)
+	{
+		this.name=name;
+		this.runs=runs;
+		this.avg=avg;
+	}
+	void disp()
+	{
+		System.out.println(name);
+		System.out.println(runs);
+		System.out.println(avg);
+	}
+	public Cricketer()
+	{
+
+	}
+	public void readExternal(ObjectInput oi )throws IOException,ClassNotFoundException
+	{
+		runs=oi.readInt();
+		avg=oi.readFloat();
+	}
+	public void writeExternal(ObjectOutput oo)throws IOException
+	{
+		oo.writeInt(runs);
+		oo.writeFloat(avg);
+	}
+}
+
+class ExternalizableSerializationLaunch
+{
+	public static void main(String[] args)throws Exception
+	{
+		Cricketer c = new Cricketer("Sachin",10000,25.2f);
+		c.disp();
+		String path="D://JavaProgram//Advance java//ioprg//ecricketer.txt";
+		FileOutputStream fos = new FileOutputStream(path);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(c);
+	}
+}
